1767317867a9542a16a29bd6f567259223987b87
祁鹏 <15035462901@163.com>
2021-05-26 23:06:25 +0800

    add routergroup

--- gee/gee.go
+++ gee/gee.go
@@ -1,30 +1,59 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
 // 	HandlerFunc defines the request handler used by gee
 type HandlerFunc func(*Context)
 
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // support middleware
+	engine      *Engine       // all groups share a Engine instance
+}
+
 // Engine implement the interface of ServerHTTP
 type Engine struct {
-	router *router
+	*RouterGroup // 将Engine作为最顶层的分组，也就是说Engine拥有RouterGroup所有的能力。
+	router       *router
+	groups       []*RouterGroup // store all groups
 }
 
 // New is the constructor of gee.Engine
 func New() *Engine {
-	return &Engine{router: NewRouter()}
+	engine := &Engine{router: NewRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
 // GET defines the method to add GET request
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.router.addRoute("GET", pattern, handler)
+func (group *RouterGroup) GET(comp string, handler HandlerFunc) {
+	group.addRoute("GET", comp, handler)
 }
 
 // POST defines the method to add POST request
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.router.addRoute("POST", pattern, handler)
+func (group *RouterGroup) POST(comp string, handler HandlerFunc) {
+	group.addRoute("POST", comp, handler)
 }
 
 // Run defines the method to start a http server
